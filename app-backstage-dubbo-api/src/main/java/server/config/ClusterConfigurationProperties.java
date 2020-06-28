391c0d44cb3b175f1b682fe2364cd1853aff6f97
ZeningZhang <13012408276@163.com>
2020-06-28 20:12:38 +0800

    提交

---
+++ app-backstage-dubbo-api/src/main/java/server/config/ClusterConfigurationProperties.java
@@ -0,0 +1,15 @@
+package server.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@ConfigurationProperties(prefix = "spring.redis.cluster")
+@Data
+public class ClusterConfigurationProperties {
+    private List<String> nodes;
+    private Integer maxRedirects;
+}
